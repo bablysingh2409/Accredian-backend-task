ab91fd675b5cc57d55c995ed8cea3417fe9bcb5e
bablysingh2409 <bablysingh2409@gmail.com>
2023-12-10 00:33:52 +0530

    Add user authentication endpoints with JWT and bcrypt

--- index.js
+++ index.js
@@ -11,78 +11,84 @@ app.use(cors());
 app.use(express.json());
 
 
-app.post('/signup',(req,res)=>{
-    const data=req.body;
 
-     db.query("Insert INTO login SET?",data,(err,result,feilds)=>{
-        if(err) err;
-        else res.send(result);
-     })
+const createToken=()=>{
+    const token= jwt.sign({_id:"234vguy65456enbfjhyjg"},"mynamejiisbvbhj",{expiresIn:"100 seconds"})
+    return token;
+}
+
+app.post('/login',async(req,res)=>{
+    const { email, password } = req.body;
+    
+    db.query("SELECT * FROM userdata WHERE email = ?", [email],async (err,results)=>{
+        if(err){
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+        else{
+           
+             const user=results[0];
+            console.log(user);
+           
+            //password comparison
+            const passwordMatch=await bcrypt.compare(password,user.password);
+            console.log(passwordMatch)
+
+            if (!passwordMatch) {
+                return res.status(401).json({ message: 'Invalid credentials' });
+            }
+
+            const token = createToken(user.id);
+            res.status(200).json({ token, user: { id: user.id, username: user.username, email: user.email } });
+            
+        }
+
+    })
+   
+   
 })
-// db.connect((err)=>{
-//     if(err){
-//         console.log('err');
-//     }
-//     else{
-//         console.log('connected')
-//     }
-// })
 
-// app.get('/',(req,res)=>{
-//     res.json({
-//         message:"a sample api",
-//     })
-// })
-
-// app.post('/login',(req,res)=>{
-//     const user={
-//         id:1,
-//         username:"anil",
-//         email:"bab@gmail.com",
-//     };
-
-//     jwt.sign({user},secretKey,{expiresIn:'300s'},(err,token)=>{
-//         res.json({token});
-//     })
-
-// })
 
 
 app.post('/signup',async(req,res)=>{
+    // console.log(req.body);
     try{
        const password=req.body.password;
        const cPassword=req.body.confirmpassword;
+       
        if(password==cPassword){
-
-        //password hash
+       
         const hashPassword=await bcrypt.hash(password,10)
+
+    
         const registerUser={
             username:req.body.username,
             email:req.body.email,
             password:hashPassword,
-            cPassword:hashPassword
+            confirmPassword:hashPassword
             
         }
 
         //data is save in data base
-        db.query("Insert INTO login SET?",registerUser,(err,result,feilds)=>{
-            if(err) err;
-            else res.send(result);
+        db.query("Insert INTO userdata SET?",registerUser,(err,result)=>{
+            if (err) {
+                console.error(err);
+                return res.status(500).send(`Error inserting into the database: ${err}`);
+            }
+            res.status(200).send(result);
          })
-        // const registered=await registerUser.save();
-        res.status(200).render('index')
+       
+        res.status(200).send(registerUser)
        }
        else{
         res.send('password are not matching')
        }
     }
     catch(err){
-       res.send('errorr',err)
+        console.error(err);
+        res.status(500).send(`Error: ${err.message || err}`);
     }
 
 })
-
-
 
 app.listen(5000,()=>{
     console.log('appp is running on 5000 port')
